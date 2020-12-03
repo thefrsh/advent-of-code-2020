4d64463172a9dce8696f705bc70e54a3db9c8a0b
Michal Broniewicz <mbroniewicz@outlook.com>
2020-12-03 18:23:03 +0100

    Part one done

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,1 +1,13 @@
+# input read
+numbers = File.open('input.txt')
+              .readlines
+              .map(&:to_i)
+
+# part one
+numbers.each do |number|
+  component = 2020 - number
+  if numbers.include?(component)
+    puts number * component
+  end
+end
 
